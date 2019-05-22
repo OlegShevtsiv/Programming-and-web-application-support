9bbadae23ed819624151f06d215bec8f440333df
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2019-05-22 22:39:28 +0300

    Update pages and app.js

--- Task2/app.js
+++ Task2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 
 mongoose.connect('mongodb://localhost/Task2Db');
@@ -8,14 +9,12 @@ let db = mongoose.connection;
 //check connection
 db.once('open', function(){
   console.log('connected to MongoDb');
-
 });
 
 
 //check for db errors
 db.on('error', function(err){
-console.log(err);
-
+  console.log(err);
 });
 
 //init App
@@ -29,6 +28,12 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//set public folder
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Home route
 app.get('/', function(req, res)
@@ -46,18 +51,46 @@ app.get('/', function(req, res)
   });
 });
 
+//get single article
+app.get('/article/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+     res.render('article', {
+       article:article
+     });
+  });
+});
+
+
+
 // add route
 app.get('/articles/add', function(req, res)
 {
   res.render('add_article', {
-    title: 'add article'
+    title: 'Add article'
   });
 });
+
+//add submit post route
+app.post('/articles/add', function(req, res){
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    }
+    else{
+      res.redirect('/');
+    }
+  });
+});
+
 
 //start server
 app.listen(3000, function()
 {
   console.log("metal forever!!!");
-
-
 });
